cfeba5c6bf9a208e5c7b08c50ea463e3894f7f30
Alexandra DeLucia <aadelucia@jhu.edu>
2020-06-02 16:16:54 -0400

    added TweetWriter

--- littlebird/__init__.py
+++ littlebird/__init__.py
@@ -1,4 +1,5 @@
 
 from .tweet_utils import TweetReader
+from .tweet_utils import TweetWriter
 from .tweet_tokenizer import TweetTokenizer
 
--- littlebird/tweet_utils.py
+++ littlebird/tweet_utils.py
@@ -24,26 +24,58 @@ class TweetReader:
     """Iterator to read a Twitter file"""
     def __init__(self, filename):
         self.path = filename
+
         try:
-            ftype = filetype.guess(filename).extension
-            if ftype == "gz":
+            self.ftype = filetype.guess(filename).extension
+        except AttributeError as err:
+            # filetype could not be determined
+            self.ftype = None
+
+        try:
+            if self.ftype == "gz":
                 self.f = gzip.open(filename, "r")
             else:
                 self.f = open(filename, "r")
         except Exception as err:
             logging.error(f"Issue opening {filename}:\n{err}")
-            return
+            sys.exit(1)
     
     def read_tweets(self):
         try:
             with jl.Reader(self.f) as reader:
                 for tweet in reader.iter(skip_empty=True, skip_invalid=True):
                     yield tweet
-        except (ValueError) as err:
-           logging.error(f"Error encountered in tweet:\n{tweet}")
-        except (gzip.BadGzipFile, zlib.error) as err:
-            logging.error(f"Error reading {self.path}: {err}")
+        except (UnicodeDecodeError, gzip.BadGzipFile, zlib.error) as err:
+            logging.error(f"Error reading {self.path} of type {self.ftype}: {err}")
+            self.f.close()
+            sys.exit(1)
+        
+        # Close file
+        self.f.close()
         return
+
+
+class TweetWriter:
+    """Write Tweets in jsonlines format"""
+    def __init__(self, filename):
+        self.path = filename
+        try:
+            if ".gz" in filename:
+                self.f = gzip.open(filename, "w+")
+            else:
+                self.f = open(filename, "w+")
+        except Exception as err:
+            logging.error(f"Issue opening {filename}:\n{err}")
+            sys.exit(1)
+    
+    def write(self, tweets):
+        """Write Tweet or list of Tweets to file"""
+        with jl.Writer(self.f) as writer:
+            if not isinstance(tweets, list):
+                writer.write(tweets)
+            else:
+                writer.write_all(tweets)
+        self.f.close()
 
 
 if __name__ == "__main__":

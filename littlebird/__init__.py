3bbf2a3bc6858988d058246d4b6d074bf995449b
Alexandra DeLucia <aadelucia@jhu.edu>
2021-12-18 18:51:09 -0500

    added BaseTweetTokenizer to init

--- littlebird/__init__.py
+++ littlebird/__init__.py
@@ -1,4 +1,4 @@
 from .constants import CONTRACTIONS
 from .tweet_utils import TweetReader
 from .tweet_utils import TweetWriter
-from .tweet_tokenizer import TweetTokenizer, GloVeTweetTokenizer, BERTweetTokenizer
+from .tweet_tokenizer import TweetTokenizer, GloVeTweetTokenizer, BERTweetTokenizer, BaseTweetTokenizer
